e32cb9c14fe8d8bd16d4c7f03bc40ef594dd4943
kd1385361 <kd1385361@st.kobedenshi.ac.jp>
2024-09-04 09:38:20 +0900

    20240902

---
+++ Sample201/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World." << endl;
+	//using namespace std;‚ðŽg‚í‚È‚¢ê‡
+	//std::cout << "Hello World." << std::endl;
+	return 0;
+}
---
+++ Sample301/car.h
@@ -0,0 +1,7 @@
+#pragma once
+
+class Car {//Carクラスの定義
+public://アクセス指定子
+	double speed;//メンバ変数
+	void drive(double hour);//メンバ関数
+};
